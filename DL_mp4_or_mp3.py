6dc1eb2f7da064e1a2ee09657e62544e902d0678
wi239144 <131501499+wi239144@users.noreply.github.com>
2023-10-12 20:07:12 +0800

    Update DL_mp4_or_mp3.py
    use title as file name

--- DL_mp4_or_mp3.py
+++ DL_mp4_or_mp3.py
@@ -15,12 +15,18 @@ yt = YouTube(
 if choose == 1 :
     #download mp3
     aud = yt.streams.filter(only_audio=True).first()
-    aud.download(filename='downloadaudio.mp3')
+    #標題當作文件名稱
+    titlenmae = yt.title + ".mp3"
+    #output:
+    aud.download(filename=titlenmae)
     print('download as mp3...')
 elif choose == 0 :
     #下載最高畫質影片
     vid = yt.streams.filter().get_highest_resolution()
-    vid.download(filename='downloadvideo.mp4')
+    #標題當作文件名稱
+    titlenmae = yt.title + ".mp4"
+    #output:
+    vid.download(filename=titlenmae)
     print('download as mp4...')
 else:
     print("Error")
